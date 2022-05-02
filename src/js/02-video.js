621329efacaa70b94326e29b884a732b33336737
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-05-03 01:19:11 +0200

    task_02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    const TIME_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay() {
+    player.getCurrentTime().then(function(seconds) {
+    localStorage.setItem(TIME_KEY, seconds)
+})
+}
+
+player.on('loaded', updateTime);
+function updateTime() {
+    const savedTime = localStorage.getItem(TIME_KEY);
+    player.setCurrentTime(savedTime).then(function(seconds) {
+        console.log(seconds);
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+    }
+});
+}
