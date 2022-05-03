11163317ba38c8bc667b77be3494e328b9119c04
Bohdan Sova <sova.bogdan.94@gmail.com>
2022-05-03 02:25:33 +0200

    task_03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+populateFormData();
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onFormSubmit);
+
+function onInputForm(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+};
+
+/* function populateFormData() {
+    const savedFormData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedFormData) {
+        form.target.value = savedFormData;
+    }
+} */
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+}
